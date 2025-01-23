f4b6117096322b1986d09a79c1535cbef73475c9
Gaurav Yadav <gauravyadav@Gauravs-MacBook-Air.local>
2025-01-24 01:01:51 +0530

    new commit

--- python-app/tests/test_app.py
+++ python-app/tests/test_app.py
@@ -1,19 +1,6 @@
-# from app import main
-# from src.app import app
+from app import main
 
-# def test_main():
-#     assert main() == "Hello, Python!"
 
-import pytest
-from src.app import app
+def test_main():
+    assert main() == "Hello, Python!"
 
-@pytest.fixture
-def client():
-    # Use Flask's test client
-    with app.test_client() as client:
-        yield client
-
-def test_index(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.data == b"Hello, world!"
