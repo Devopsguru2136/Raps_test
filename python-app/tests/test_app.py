51e7cc7c946931f5c1f751d642955e6d20ba565b
Gaurav Yadav <gauravyadav@Gauravs-MacBook-Air.local>
2025-01-24 00:48:09 +0530

    new commit

--- python-app/tests/test_app.py
+++ python-app/tests/test_app.py
@@ -1,12 +1,4 @@
-import pytest
-from src.app import app
+from app import main
 
-@pytest.fixture
-def client():
-    with app.test_client() as client:
-        yield client
-
-def test_index(client):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.data == b"Hello, world!"
+def test_main():
+    assert main() == "Hello, Python!"
